417539e314ff8a325d6e5a3f0ebfb1108cce0518
LivingW123 <onealwang0202@gmail.com>
2022-08-21 10:39:39 -0700

    layout and attempted signin

--- app/src/main/java/com/example/sentimentanalysis/ProfileActivity.java
+++ app/src/main/java/com/example/sentimentanalysis/ProfileActivity.java
@@ -2,16 +2,76 @@ package com.example.sentimentanalysis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button googleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        googleBtn = this.findViewById(R.id.google_button);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount act = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(act != null){
+            doCoolStuff();
+        }
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
     }
-    
+
+    void signIn() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                doCoolStuff();
+            } catch (ApiException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "oops", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void doCoolStuff(){
+        GoogleSignInAccount act = GoogleSignIn.getLastSignedInAccount(this);
+        String personName = act.getDisplayName();
+        String personEmail = act.getEmail();
+        Toast.makeText(getApplicationContext(), personName, Toast.LENGTH_LONG).show();
+        googleBtn.setText("yay");
+    }
+
+
 
 
 
