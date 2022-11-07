1b4b858b44e114853b3edfa1d19213e01a66eeee
LivingW123 <onealwang0202@gmail.com>
2022-11-06 19:29:34 -0800

    begin ibm connect and minor chat ui

--- app/src/main/java/com/example/sentimentanalysis/ChatActivity.java
+++ app/src/main/java/com/example/sentimentanalysis/ChatActivity.java
@@ -3,13 +3,20 @@ package com.example.sentimentanalysis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    TextView UserInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        UserInput=findViewById(R.id.MessageTextView);
+
     }
 }
+
