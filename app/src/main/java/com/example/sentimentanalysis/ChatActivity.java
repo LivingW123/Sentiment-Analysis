582cc31822d6e7f935218afbf3d790e67f050e03
EthanLDot <ethanlee2337@gmail.com>
2022-11-08 21:54:10 +0900

    changed ui elements after ibm repo

--- app/src/main/java/com/example/sentimentanalysis/ChatActivity.java
+++ app/src/main/java/com/example/sentimentanalysis/ChatActivity.java
@@ -8,14 +8,10 @@ import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
 
-    TextView UserInput;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
-        UserInput=findViewById(R.id.MessageTextView);
 
     }
 }
